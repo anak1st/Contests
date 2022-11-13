59d5ec0265f8ad13f3d37979286dabbb1ac95ccd
Anak1st <xiafan8050@outlook.com>
2022-11-13 23:24:36 +0800

    icpc 2021 sy

--- include/templates/data_structure/dsu.hpp
+++ include/templates/data_structure/dsu.hpp
@@ -9,21 +9,16 @@ private:
     std::vector<int> f, s;
 
 public:
-    dsu(int n) : f(n), s(n, 1) {
-        std::iota(f.begin(), f.end(), 0);
-    }
-
+    dsu(int n) : f(n), s(n, 1) { std::iota(f.begin(), f.end(), 0); }
+    
     int find(int x) {
         if (f[x] != x) {
             f[x] = find(f[x]);
         }
         return f[x];
     }
-
-    bool same(int x, int y) {
-        return find(x) == find(y);
-    }
-
+    bool same(int x, int y) { return find(x) == find(y); }
+    
     bool merge(int x, int y) {
         x = find(x);
         y = find(y);
@@ -32,8 +27,6 @@ public:
         f[y] = x;
         return true;
     }
-
-    int size(int x) {
-        return s[find(x)];
-    }
+    
+    int size(int x) { return s[find(x)]; }
 };
--- src/xcpc/c/2022-weihai/C.cpp
+++ src/xcpc/c/2022-weihai/C.cpp
@@ -1,44 +1,27 @@
 #include <bits/stdc++.h>
 
-using namespace std;
-
 using i64 = long long;
-using u64 = unsigned long long;
 
 std::pair<int, int> pgcd(int x, int y) {
-    int g = abs(gcd(x, y));
+    int g = abs(std::gcd(x, y));
     return {x / g, y / g};
 }
 
-const int N = 25010;
-std::vector<std::pair<int, int>> p(N);
-int n;
-
-bool valid_permutation(vector<int>& v) {
-    set<pair<int, int>> S;
-    for (int i = 1; i < 5; i++) {
-        auto [x0, y0] = p[v[0]];
-        auto [xi, yi] = p[v[i]];
-        auto dx = xi - x0, dy = yi - y0;
-        S.insert(pgcd(dx, dy));
-    }
-    if (S.size() == 4) return true;
-    return false;
-}
-
 void solve() {
-    cin >> n;
+    int n;
+    std::cin >> n;
+    std::vector<std::pair<int, int>> p(n);
     for (int i = 0; i < n; i++) {
         auto& [xi, yi] = p[i];
-        cin >> xi >> yi;
+        std::cin >> xi >> yi;
     }
 
     {
         if (n < 5) {
-            cout << "NO\n";
+            std::cout << "NO\n";
             return;
         }
-        set<pair<int, int>> S;
+        std::set<std::pair<int, int>> S;
         for (int i = 1; i < n; i++) {
             auto [x0, y0] = p[0];
             auto [xi, yi] = p[i];
@@ -47,15 +30,15 @@ void solve() {
             S.insert(pgcd(dx, dy));
         }
         if (S.size() == 1) {
-            cout << "NO\n";
+            std::cout << "NO\n";
             return;
         }
     }
-    
-    vector<int> v = {0, 1, 2, 3};
+
+    std::vector<int> v = {0, 1, 2, 3};
 
     for (int i = 4; i < n; i++) {
-        set<pair<int, int>> S;
+        std::set<std::pair<int, int>> S;
         for (int j = 0; j < 4; j++) {
             auto [xi, yi] = p[i];
             auto [xj, yj] = p[j];
@@ -70,7 +53,7 @@ void solve() {
     }
 
     for (int i = 0; i < 5; i++) {
-        set<pair<int, int>> st;
+        std::set<std::pair<int, int>> st;
         auto [xi, yi] = p[v[i]];
         for (int j = 0; j < 5; j++) {
             if (v[i] == v[j]) {
@@ -95,9 +78,11 @@ void solve() {
     }
 }
 
-signed main() {
-    int T;
-    cin >> T;
-    while (T--) solve();
+int main() {
+    int tt;
+    std::cin >> tt;
+    while (tt--) {
+        solve();
+    }
     return 0;
 }
