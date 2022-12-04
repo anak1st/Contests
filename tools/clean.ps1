f79841599897952a077588a9a2b925150496e1fb
Anak1st <xiafan8050@outlook.com>
2022-12-04 20:17:25 +0800

    latex linux

--- tools/clean.ps1
+++ tools/clean.ps1
@@ -1,6 +1,5 @@
 Remove-Item .\build\cph\*
 Remove-Item .\assets\docs\*.aux
-Remove-Item .\assets\docs\*.aux
 Remove-Item .\assets\docs\*.fdb_latexmk
 Remove-Item .\assets\docs\*.fls
 Remove-Item .\assets\docs\*.log
---
+++ tools/clean.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/zsh
+
+rm ./build/cph/*
+rm ./assets/docs/*.aux
+rm ./assets/docs/*.fdb_latexmk
+rm ./assets/docs/*.fls
+rm ./assets/docs/*.log
+rm ./assets/docs/*.out
+rm ./assets/docs/*.synctex.gz
+rm ./assets/docs/*.xdv
