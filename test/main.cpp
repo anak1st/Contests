b6d1fe938c0251f445a2ce94496d28bdbacd9098
Anak1st <xiafan8050@outlook.com>
2023-01-28 11:53:10 +0800

    clangd

--- test/main.cpp
+++ test/main.cpp
@@ -3,6 +3,8 @@
  * @date: 2023-01-27 21:57
  **/
 #include <bits/stdc++.h>
+#include <vector>
+#include <debug/vector>
 using i64 = long long;
 
 int main() {
@@ -10,6 +12,11 @@ int main() {
     std::cin.tie(nullptr);
 
     std::cout << __cplusplus << "\n";
+    std::vector<std::string> a;
+    for (int i = 1; i <= 9; i++) {
+        a.emplace_back(i, '0' + i);
+    }
+    std::cout << a.size() << "\n";
 
     return 0;
 }
