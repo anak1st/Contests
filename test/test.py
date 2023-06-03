f8e7defc3c697e8e6150cc9a7684cf6ede65b36d
Anak1st <xiafan8050@outlook.com>
2023-06-03 23:33:48 +0800

    path

--- test/test.py
+++ test/test.py
@@ -1,22 +1,4 @@
-import numpy as np
-
-def fab(x : int) -> int:
-    if x < 1:
-        return 1
-    return fab(x-1) + fab(x-2)
+import math
 
 if __name__ == "__main__":
-    while True:
-        s = input()
-        if len(s) == 0:
-            break
-        # remove all blank space
-        s = s.replace(" ", "")
-        s = s.replace("\t", "")
-        s = s.replace("\r", "")
-        s = s.replace("\n", "")
-
-        if len(s) == 0:
-            break
-
-        print(s, end="")
+    print(math.pi)
--- tools/precompile.ps1
+++ tools/precompile.ps1
@@ -1,10 +1,12 @@
-$mingwDir = "C:\Development\msys2\ucrt64"
-$header = "$mingwDir\include\c++\12.2.0\x86_64-w64-mingw32\bits\stdc++.h"
-g++ $header -std=c++20 -O2
-Copy-Item $header .\build\stdc++.h
-g++ .\build\stdc++.h -std=c++20 -g
+$mingwDir = "C:\Development\msys64\ucrt64"
+
+$header    = "$mingwDir\include\c++\13.1.0\x86_64-w64-mingw32\bits\stdc++.h"
+$gchFolder = "$mingwDir\include\c++\13.1.0\x86_64-w64-mingw32\bits\stdc++.h.gch"
+
+g++ $header -std=c++20 -O2 -o $gchFolder\stdc++.h.release.gch
+g++ $header -std=c++20 -g  -o $gchFolder\stdc++.h.debug.gch
 
 $binFolder = "$mingwDir\bin"
-Copy-Item $binFolder\libstdc++-6.dll .\build\libstdc++-6.dll
-Copy-Item $binFolder\libgcc_s_seh-1.dll .\build\libgcc_s_seh-1.dll
+Copy-Item $binFolder\libstdc++-6.dll     .\build\libstdc++-6.dll
+Copy-Item $binFolder\libgcc_s_seh-1.dll  .\build\libgcc_s_seh-1.dll
 Copy-Item $binFolder\libwinpthread-1.dll .\build\libwinpthread-1.dll
