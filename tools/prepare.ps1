3f6aeffbc2144f50625d819ea7c8744bfb1a0e04
Anak1st <87300894+anak1st@users.noreply.github.com>
2023-07-23 23:10:15 +0800

    settings

--- tools/prepare.ps1
+++ tools/prepare.ps1
@@ -5,6 +5,6 @@ Copy-Item $binFolder\libstdc++-6.dll     .\build\libstdc++-6.dll
 Copy-Item $binFolder\libgcc_s_seh-1.dll  .\build\libgcc_s_seh-1.dll
 Copy-Item $binFolder\libwinpthread-1.dll .\build\libwinpthread-1.dll
 
-$allHeader = "$mingwDir\include\c++\13.1.0\x86_64-w64-mingw32\bits\stdc++.h"
+# $allHeader = "$mingwDir\include\c++\13.1.0\x86_64-w64-mingw32\bits\stdc++.h"
 # Copy-Item $AllHeader .\include\bits\stdc++.h
 g++ .\include\bits\stdc++.h -std=c++20 -O2 -static
--- tools/test.ps1
+++
@@ -1,4 +0,0 @@
-for ($i = 1; $i -le 100; $i++ ) {
-    Write-Progress -Activity "Search in Progress" -Status "$i% Complete:" -PercentComplete $i
-    Start-Sleep -Milliseconds 250
-}
